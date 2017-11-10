0195a26bf2abb5f0dfd44789c8d1e513230a2c1a
Abraham Arce <xe1gyq@gmail.com>
2017-11-10 19:12:26 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- smarthat/main.py
+++ smarthat/main.py
@@ -4,11 +4,11 @@ from upm import pyupm_grove as grove
 from upm import pyupm_buzzer as upmBuzzer
 from upm import pyupm_ultrasonic as upmUltraSonic
 from upm import pyupm_gp2y0a as upmGp2y0a
-from upm import pyupm_hcsr04 as hcsr04
 
-def buzzerFunction():
+def buzzerFunction(long):
 
     buzzer = upmBuzzer.Buzzer(5)
+    print long
 
     chords = [upmBuzzer.BUZZER_DO, upmBuzzer.BUZZER_RE, upmBuzzer.BUZZER_MI,
               upmBuzzer.BUZZER_FA, upmBuzzer.BUZZER_SOL, upmBuzzer.BUZZER_LA,
@@ -16,22 +16,16 @@ def buzzerFunction():
 
     for chord_ind in range (0,1):
         # play each note for a half second
-        print(buzzer.playSound(chords[chord_ind], 500000))
+        print(buzzer.playSound(chords[chord_ind], long))
         time.sleep(0.1)
-
-    print("exiting application")
 
     del buzzer
 
 if __name__ == '__main__':
 
-    ultrasonic = upmUltraSonic.UltraSonic(2)
+    ultrasonic = upmUltraSonic.UltraSonic(8)
     knob = grove.GroveRotary(3)
-    myIRProximity = upmGp2y0a.GP2Y0A(2)
-    hcsr04_sensor = hcsr04.HCSR04(6, 7);
-
-    GP2Y0A_AREF = 5.0;
-    SAMPLES_PER_QUERY = 20;
+    myIRProximity = upmGp2y0a.GP2Y0A(1)
 
     while True:
         
@@ -40,12 +34,21 @@ if __name__ == '__main__':
         distance = ultrasonic.getDistance()
 
         print("Knob Abs: %4d" % int(abs) , "Ultrasonic Distance %4d" % int(distance))
-        print("Distance: {0}".format(hcsr04_sensor.getDistance(hcsr04.HCSR04_CM)))
 
-        #if ( int(distance) <= int(abs) ):
-        #    buzzerFunction()
-    
-        #print myIRProximity.value(GP2Y0A_AREF, SAMPLES_PER_QUERY)
+        threshold = int(abs) * 2
+        print threshold
+
+        if int(distance) in xrange(0,1000):
+            print 'One'
+            buzzerFunction(threshold*50)
+        elif int(distance) in xrange(1000, 2500):
+            print 'Two'
+            buzzerFunction(threshold*25)
+        elif int(distance) in xrange(2500,4000):
+            print 'Three'
+            buzzerFunction(threshold)
+        else:
+            print 'Four'
 
         time.sleep(.1)
 
